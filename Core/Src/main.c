2b07cbce84585f2349c0e87e192d273e19d3c0ee
Joris Witteman <joriswitteman@hotmail.com>
2020-09-26 22:04:55 +0200

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,620 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "tusb.h"
+//#include "i2c_handler.h"
+#include "usb_descriptors.h"
+//#include "button_driver.h"
+//#include "led_driver.h"
+#include "uart_driver.h"
+//#include "interrupt_pin.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define URL  "webusb.hackz.one"
+
+const tusb_desc_webusb_url_t desc_url =
+{
+  .bLength         = 3 + sizeof(URL) - 1,
+  .bDescriptorType = 3, // WEBUSB URL type
+  .bScheme         = 1, // 0: http, 1: https
+  .url             = URL
+};
+
+static bool web_serial_connected = false;
+void webserial_task(void);
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+DMA_HandleTypeDef hdma_usart2_tx;
+
+PCD_HandleTypeDef hpcd_USB_FS;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_USB_PCD_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+enum  {
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+};
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+uint32_t last_tick;
+
+void led_task(void);
+void usb_device_task(void);
+void cdc_task(void);
+void hid_task(void);
+void midi_task(void);
+uint32_t board_millis(void);
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+#ifdef BOOTLOADER_BUILD
+	SCB->VTOR = 0x08004000;
+	__enable_irq();
+#endif
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  MX_USB_PCD_Init();
+  /* USER CODE BEGIN 2 */
+  //HAL_I2C_EnableListen_IT(&hi2c1);
+  uart_init();
+  tusb_init();
+
+  //init_led(&hspi2, &htim1);
+  //button_init();
+
+  //HAL_TIM_PWM_Start_IT(&htim1, TIM_CHANNEL_1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+	  	tud_task(); // tinyusb device task
+		#if CFG_TUD_CDC
+				cdc_task();
+		#endif
+
+		#if CFG_TUD_HID
+		//		hid_task();
+		#endif
+
+		#if CFG_TUD_MIDI
+		//		midi_task();
+		#endif
+
+		//webusb_task();
+
+		//button_task();
+
+		//i2c_watchdog(&hi2c1);
+
+		if(last_tick != HAL_GetTick()) {
+			last_tick = HAL_GetTick();
+			if (UART_SERIAL.Instance->SR & UART_FLAG_IDLE && UART_SERIAL.hdmarx->State == HAL_DMA_STATE_BUSY) /* if Idle flag is set */
+			{
+				volatile uint32_t tmp; /* Must be volatile to prevent optimizations */
+				tmp = UART_SERIAL.Instance->SR; /* Read status register */
+				tmp = UART_SERIAL.Instance->DR; /* Read data register */ //This two reads clears the IDLE Flag
+				UNUSED(tmp);
+
+				uint32_t cndtr = UART_SERIAL.hdmarx->Instance->CNDTR;
+				if(cndtr < 256) {
+					HAL_UART_AbortReceive(&UART_SERIAL);
+					UART_Early_Exit(&UART_SERIAL, cndtr);
+				}
+			}
+
+			if(UART_SERIAL.hdmarx->State == HAL_DMA_STATE_READY) {
+				UART_Reset();
+			}
+
+//			if (UART_WEBUSB.Instance->SR & UART_FLAG_IDLE && UART_WEBUSB.hdmarx->State == HAL_DMA_STATE_BUSY) /* if Idle flag is set */
+//			{
+//				volatile uint32_t tmp; /* Must be volatile to prevent optimizations */
+//				tmp = UART_WEBUSB.Instance->SR; /* Read status register */
+//				tmp = UART_WEBUSB.Instance->DR; /* Read data register */ //This two reads clears the IDLE Flag
+//				UNUSED(tmp);
+//
+//				uint32_t cndtr = UART_WEBUSB.hdmarx->Instance->CNDTR;
+//				if(cndtr < 256) {
+//					HAL_UART_AbortReceive(&UART_WEBUSB);
+//					UART_Early_Exit(&UART_WEBUSB, cndtr);
+//				}
+//			}
+//
+//			if(UART_WEBUSB.hdmarx->State == HAL_DMA_STATE_READY) {
+//				WebUSB_Reset();
+//			}
+		}
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV2;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief USB Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USB_PCD_Init(void)
+{
+
+  /* USER CODE BEGIN USB_Init 0 */
+
+  /* USER CODE END USB_Init 0 */
+
+  /* USER CODE BEGIN USB_Init 1 */
+
+  /* USER CODE END USB_Init 1 */
+  hpcd_USB_FS.Instance = USB;
+  hpcd_USB_FS.Init.dev_endpoints = 8;
+  hpcd_USB_FS.Init.speed = PCD_SPEED_FULL;
+  hpcd_USB_FS.Init.low_power_enable = DISABLE;
+  hpcd_USB_FS.Init.lpm_enable = DISABLE;
+  hpcd_USB_FS.Init.battery_charging_enable = DISABLE;
+  if (HAL_PCD_Init(&hpcd_USB_FS) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_Init 2 */
+
+  /* USER CODE END USB_Init 2 */
+
+}
+
+/** 
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void) 
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel6_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel6_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel6_IRQn);
+  /* DMA1_Channel7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel7_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel7_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : PC13 PC14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+  blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void) remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+#if CFG_TUD_HID
+
+//void hid_task(void)
+//{
+//	bool written = false;
+//	bool should_pause = false;
+//	uint16_t* button_state = (uint16_t*) getI2CMemory(4);
+//	uint8_t* hid_keyboard_dirty = (uint8_t*) getI2CMemory(71);
+//	uint8_t* hid_mouse_dirty = (uint8_t*) getI2CMemory(77);
+//
+//	// Perform wake-up over USB if any button is pressed
+//	if ( tud_suspended() && *button_state )
+//	{
+//		// Wake up host if we are in suspend mode
+//		// and REMOTE_WAKEUP feature is enabled by host
+//		tud_remote_wakeup();
+//	}
+//
+//	if(*hid_keyboard_dirty && tud_hid_ready()) {
+//		uint8_t* key_modifier = (uint8_t*) getI2CMemory(64);
+//		uint8_t* keycodes = (uint8_t*) getI2CMemory(65); // 6 bytes
+//		tud_hid_keyboard_report(REPORT_ID_KEYBOARD, *key_modifier, keycodes);
+//
+//		*hid_keyboard_dirty = 0;
+//		written = true;
+//		should_pause = true;
+//	}
+//
+//	if(*hid_mouse_dirty && tud_hid_ready()) {
+//		if(should_pause) {
+//			// delay a bit before attempt to send mouse report
+//			HAL_Delay(10);
+//		}
+//
+//		int8_t* mouse_data = (int8_t*) getI2CMemory(72);
+//
+//		// Data in order: buttons bitmask, X, Y, scroll, pan
+//		tud_hid_mouse_report(REPORT_ID_MOUSE, *((uint8_t*)mouse_data), *(mouse_data+1), *(mouse_data+2), *(mouse_data+3), *(mouse_data+4));
+//		*hid_mouse_dirty = 0;
+//		written = true;
+//	}
+//
+//	if(written) {
+//		addInterruptReason(INTERRUPT_REASON_HID_WRITTEN);
+//		setInterruptPin();
+//	}
+//}
+
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+{
+  // TODO not Implemented
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) reqlen;
+
+  return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize)
+{
+  // TODO set LED based on CAPLOCK, NUMLOCK etc...
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) bufsize;
+}
+
+#endif
+
+//--------------------------------------------------------------------+
+// MIDI Task
+//--------------------------------------------------------------------+
+
+#if CFG_TUD_MIDI
+
+//void midi_task(void)
+//{
+//	bool written = false;
+//
+//	// Check and send MIDI data in any of the 4 data buffers
+//	for(uint8_t i = 0; i < 4; i++) {
+//		uint8_t* midi_data = (uint8_t*) getI2CMemory(78 + i * 4);
+//		uint8_t* dirty_byte = midi_data + 3;
+//
+//		if(*dirty_byte) {
+//			tudi_midi_write24(0, *midi_data, *(midi_data+1), *(midi_data+2));
+//			*dirty_byte = 0;
+//			written = true;
+//		}
+//	}
+//
+//	if(written) {
+//		addInterruptReason(INTERRUPT_REASON_MIDI_WRITTEN);
+//		setInterruptPin();
+//	}
+//
+//	if(tud_midi_available()) {
+//		uint8_t* data = getI2CMemory(95);
+//		uint8_t* count = getI2CMemory(94);
+//		*count = tud_midi_read(data, 32);
+//		addInterruptReason(INTTERUPT_REASON_MIDI_RECEIVED);
+//		setInterruptPin();
+//		//Do something with the data here
+//
+//	}
+//}
+
+
+#endif
+
+//--------------------------------------------------------------------+
+// WebUSB use vendor class
+//--------------------------------------------------------------------+
+#if TUD_VENDOR
+// Invoked when received VENDOR control request
+bool tud_vendor_control_request_cb(uint8_t rhport, tusb_control_request_t const * request)
+{
+  switch (request->bRequest)
+  {
+    case VENDOR_REQUEST_WEBUSB:
+      // match vendor request in BOS descriptor
+      // Get landing page url
+      return tud_control_xfer(rhport, request, (void*) &desc_url, desc_url.bLength);
+
+    case VENDOR_REQUEST_MICROSOFT:
+      if ( request->wIndex == 7 )
+      {
+        // Get Microsoft OS 2.0 compatible descriptor
+        uint16_t total_len;
+        memcpy(&total_len, desc_ms_os_20+8, 2);
+
+        return tud_control_xfer(rhport, request, (void*) desc_ms_os_20, total_len);
+      }else
+      {
+        return false;
+      }
+
+    case 0x22:
+      // Webserial simulate the CDC_REQUEST_SET_CONTROL_LINE_STATE (0x22) to
+      // connect and disconnect.
+      web_serial_connected = (request->wValue != 0);
+
+      // Always lit LED if connected
+      if ( web_serial_connected )
+      {
+        //board_led_write(true);
+        //blink_interval_ms = BLINK_ALWAYS_ON;
+
+
+      }else
+      {
+        blink_interval_ms = BLINK_MOUNTED;
+      }
+
+      // response with status OK
+      return tud_control_status(rhport, request);
+
+    default:
+      // stall unknown request
+      return false;
+  }
+
+  return true;
+}
+
+// Invoked when DATA Stage of VENDOR's request is complete
+bool tud_vendor_control_complete_cb(uint8_t rhport, tusb_control_request_t const * request)
+{
+  (void) rhport;
+  (void) request;
+
+  // nothing to do
+  return true;
+}
+#endif
+
+uint32_t board_millis(void)
+{
+  return HAL_GetTick();
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
